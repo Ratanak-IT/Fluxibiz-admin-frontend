9859de4c807b9d659a21597b7c6bb3b7d58a665f
ratanak <ratanakcoding.it@gmail.com>
2026-07-24 15:51:29 +0700

    update validation

--- src/lib/schemas/loginSchema.ts
+++ src/lib/schemas/loginSchema.ts
@@ -6,9 +6,12 @@ export const loginSchema = z.object({
     .min(1, { message: "Email is required" })
     .email({ message: "Invalid email address" }),
   password: z
-    .string()
-    .min(1, { message: "Password is required" })
-    .min(6, { message: "Password must be at least 6 characters" }),
+  .string()
+  .min(1, { message: "Password is required" })
+  .min(8, { message: "Password must be at least 8 characters" })
+  .regex(/^(?=.*[a-zA-Z])(?=.*\d)/, {
+    message: "Password must contain both letters and numbers",
+  }),
 });
 
 export type LoginFormType = z.infer<typeof loginSchema>;
